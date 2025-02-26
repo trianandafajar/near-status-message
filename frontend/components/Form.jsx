b97e37a3005304078bdbeae68dbc8b337d2a3f1d
trianandafajar <mail.trianandafajar@gmail.com>
2025-02-26 11:06:26 +0700

    Add text

--- frontend/components/Form.jsx
+++ frontend/components/Form.jsx
@@ -5,7 +5,7 @@ export default function Form({ onSubmit, currentUser }) {
   return (
     <form onSubmit={onSubmit}>
       <fieldset id="fieldset">
-        <p>Add or update your status message!</p>
+        <p>Add or update your status message!!</p>
         <p className="highlight">
           <label htmlFor="message">Message:</label>
           <input
